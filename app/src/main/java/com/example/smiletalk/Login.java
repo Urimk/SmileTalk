6065e299750eb4b4ae5314e1d54f38526648f8ea
shmini31 <shmini31@gmail.com>
2023-06-14 19:37:26 +0300

    finish design login and signin

---
+++ app/src/main/java/com/example/smiletalk/Login.java
@@ -0,0 +1,21 @@
+package com.example.smiletalk;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.smiletalk.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/smiletalk/SighIn.java
@@ -0,0 +1,14 @@
+package com.example.smiletalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SighIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
