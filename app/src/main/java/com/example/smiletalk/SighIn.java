7197aed65a4a0ef0f0fe320073b3600ceebefccb
shmini31 <shmini31@gmail.com>
2023-06-15 09:16:35 +0300

    fix_design

--- app/src/main/java/com/example/smiletalk/SighIn.java
+++ app/src/main/java/com/example/smiletalk/SighIn.java
@@ -9,6 +9,7 @@ public class SighIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
+
     }
 }
