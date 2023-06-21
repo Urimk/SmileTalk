ceadcadec18218b5bc753c87dd261c91b4af86ad
Uri Knoll <umkil99@gmail.com>
2023-06-21 17:27:16 +0300

    Can now send message. Now server side. then I would be able to fix second delete and to add the first intro message to each chat

---
+++ app/src/main/java/com/example/smiletalk/MessageAdapter.java
@@ -0,0 +1,66 @@
+package com.example.smiletalk;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private List<Message> messageList;
+
+    public MessageAdapter(List<Message> messageList) {
+        this.messageList = messageList;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_sent, parent, false);
+        return new MessageViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messageList.get(position);
+
+        holder.messageTextView.setText(message.getContent());
+        holder.timestampTextView.setText(message.getTime());
+
+        holder.messageTextView.setGravity(Gravity.BOTTOM);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public void addMessage(Message message) {
+        messageList.add(message);
+        notifyDataSetChanged();
+    }
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageTextView;
+        TextView timestampTextView;
+
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+            messageTextView = itemView.findViewById(R.id.messageTextView);
+            timestampTextView = itemView.findViewById(R.id.timestampTextView);
+        }
+    }
+}
+
+
+
+
